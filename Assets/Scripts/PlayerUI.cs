3dc2206ebfe1c263552c3cda6e42ac7951f31090
Max V [102] <107592271+MV1027774sae@users.noreply.github.com>
2023-04-27 15:52:27 +1000

    Added win trigger

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -6,10 +6,22 @@ using TMPro;
 public class PlayerUI : MonoBehaviour
 {
     public int treasureCollected = 0;
+
     [SerializeField] private TextMeshProUGUI treasureText;
+    [SerializeField] private GameObject winTrigger;
+
+    private void Start()
+    {
+        winTrigger.SetActive(false);
+    }
 
     void Update()
     {
         treasureText.text = treasureCollected.ToString() + " / 4";
+
+        if (treasureCollected >= 4)
+        {
+            winTrigger.SetActive(true);
+        }
     }
 }
